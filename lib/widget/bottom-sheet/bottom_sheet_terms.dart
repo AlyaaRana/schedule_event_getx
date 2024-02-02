4382e554c76477864b7cd658f31b0d4178a2b28d
Alyaa Rana Raya <alyaaranaraya@gmail.com>
2024-02-02 13:27:01 +0700

    swipeable terms & condition

---
+++ lib/widget/bottom-sheet/bottom_sheet_terms.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/material.dart';
+import 'package:schedule_event_getx/helper/themes.dart';
+import 'package:schedule_event_getx/widget/swipe-button/swipeable_button.dart';
+
+class BottomSheetTerms extends StatelessWidget {
+  const BottomSheetTerms({super.key});
+
+  static Widget buildBottomSheetTerms(BuildContext context, double screenWidth, double screenHeight) {
+    return BottomSheet(
+      onClosing: () {},
+      backgroundColor: Colors.transparent,
+      enableDrag: false,
+      builder: (context) {
+        return ClipRRect(
+          child: Container(
+            height: 360,
+            decoration: BoxDecoration(
+              color: muteGrey,
+              borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(30),
+                topRight: Radius.circular(30),
+              ),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.white.withOpacity(0.25),
+                  spreadRadius: 0,
+                  blurRadius: 15,
+                ),
+              ],
+            ),
+            width: screenWidth,
+            child: Container(
+              padding: const EdgeInsets.only(top: 15, left: 16, right: 16),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Center(
+                    child: Container(
+                      width: 36,
+                      height: 5, // Height of the horizontal line
+                      color: containerGrey, // Color of the horizontal line
+                    ),
+                  ),
+                  SizedBox(height: 20,),
+                  Text("Terms & Conditions", style: txtTitleTerms(),),
+                  SizedBox(height: 10),
+                  Text("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi tortor ligula, rhoncus eu molestie sed, consectetur vitae nisi. Aenean semper, dui vehicula imperdiet scelerisque, urna lorem tincidunt erat, a mollis augue velit nec velit. Praesent vitae sodales leo. Phasellus volutpat velit lorem, a consectetur turpis cursus in. Pellentesque a magna ac tortor venenatis maximus at eu neque. Nunc bibendum sem nec sem suscipit, ut faucibus ipsum dictum. Pellentesque sollicitudin dui magna, in maximus est mollis ut. Donec semper leo sit amet fringilla tempor. ", style: caption3(),),
+                  Center(
+                    child: Container(
+                      padding: const EdgeInsets.only(top: 30),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          SwipeableButton(),
+                          SizedBox(height: 15,),
+                          Text("Cancel", style: subtitle3(),)
+                        ],
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ),
+        );
+      },
+    );
+  }
+
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
+
+
+
+
