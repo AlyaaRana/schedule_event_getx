5a9db8b25e9557a5fb57c6ecb317c67b93bf2bad
Alyaa Rana Raya <alyaaranaraya@gmail.com>
2024-02-10 08:50:57 +0700

    screen

---
+++ lib/view/event_add_view.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class AddTitle extends StatelessWidget {
+  const AddTitle({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+
+        ],
+      ),
+    );
+  }
+}
